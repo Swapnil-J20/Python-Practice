7ec6919054dca0482647b022676953615c094446
Swapnil Jadhav <jswapnil.id@gmail.com>
2024-11-16 22:58:52 +0530

    Commit 4

---
+++ 03_Variables_and_Data_Types/variables_and_data_types.py
@@ -0,0 +1,23 @@
+num = 11
+
+print(f"The value is {num}")
+print(f"The data type is {type(num)}")
+print(f"ID of no is {id(num)}\n")
+
+deci = 3.71
+
+print(f"The value is {deci}")
+print(f"The data type is {type(deci)}")
+print(f"ID of no is {id(deci)}\n")
+
+text = "Hello"
+
+print(f"The value is {text}")
+print(f"The data type is {type(text)}")
+print(f"ID of no is {id(text)}\n")
+
+choice = True
+
+print(f"The value is {choice}")
+print(f"The data type is {type(choice)}")
+print(f"ID of no is {id(choice)}")
---
+++ 04_Main/main.py
@@ -0,0 +1,16 @@
+# Outside main domain
+print("First line: Zeroth Indentation")
+
+# defining main function
+def main():
+
+	print("Second line: First Indentation")
+	print("Inside main")
+
+# Program Starter
+if __name__ == "__main__":
+	print("Inside Starter")
+	# function call
+	main()
+
+print("Third line: End of program")
