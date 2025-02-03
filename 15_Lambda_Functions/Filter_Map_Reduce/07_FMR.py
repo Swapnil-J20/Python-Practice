05234711edc29adf3f4a152585a9cca738f2ae3f
Swapnil Jadhav <jswapnil.id@gmail.com>
2025-02-03 23:58:04 +0530

    Commit 66

--- 15_Lambda_Functions/Filter_Map_Reduce/07_FMR.py
+++ 15_Lambda_Functions/Filter_Map_Reduce/07_FMR.py
@@ -1,38 +1,37 @@
-from functools import reduce
+from functools import reduce  # Importing reduce function from functools
 
-CheckEven = lambda No:(No % 2 == 0)
+# Lambda function to check if a number is even
+CheckEven = lambda No: (No % 2 == 0)
 
+# Lambda function to double a number
 Doubles = lambda No: No * 2
 
+# Lambda function to sum two numbers
+Sum = lambda A, B: A + B 
+
 def main():
-	print("Enter number of elements you want to enter : ")
-    iSize = int(input())
+    print("Enter number of elements you want to enter: ")
+    iSize = int(input())  # Taking input for the number of elements
 
     Data_Input = []
     print("Please enter the data ")
-    for iCnt in range(0,iSize,1):
-        Value = int(input())
-        Data_Input.append(Value)
+    for iCnt in range(0, iSize, 1):
+        Value = int(input())  # Taking integer input
+        Data_Input.append(Value)  # Adding input to the list
 
-    print("Data is : ",Data_Input)
+    print("Data is:", Data_Input)
 
+    # Filtering even numbers from the input data
     Data_Filter = list(filter(CheckEven, Data_Input))
+    print("Data after filter is:", Data_Filter)
 
-    print("Data after filter is : ",Data_Filter)
-
+    # Mapping: Doubling each even number
     Data_Map = list(map(Doubles, Data_Filter))
+    print("Data after map is:", Data_Map)
 
-    print("Data after map is : ",Data_Map)
-
-
-
-
-
-
-
-
-
-
+    # Reducing: Summing up all doubled values
+    Output = reduce(Sum, Data_Map)
+    print("Result after reduce is:", Output)
 
 if __name__ == "__main__":
-	main()
+    main()  # Entry point of the program
